2ae27982b9ff9a047892c09d1aafeec7649c8411
sunwoo <ajtnlaos94@naver.com>
2020-09-30 14:49:04 +0900

    alter : readme

--- Web/html/js/index.js
+++
@@ -1,3 +0,0 @@
-function logout(){
-    window.location.href='http://tkdl2401.cafe24.com/html/php/logout.php'
-}
--- Web/html/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-?>
-<script>
-window.location.href='http://tkdl2401.cafe24.com/html/login.html';
-</script>
